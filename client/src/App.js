c3f030ce2b6a499933228636a569fea5eeb06f5e
Merel Jacobs <merel.burleigh@gmail.com>
2024-04-15 14:32:53 -0700

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Profile from './components/Profile'; 
+import NotFound from "./pages/Error/NotFound";
 
 function App() {
-    const [ user, setUser ] = useState([]);
-    const [ profile, setProfile ] = useState(null);
+  const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
 
-    const login = useGoogleLogin({
-        onSuccess: (codeResponse) => setUser(codeResponse),
-        onError: (error) => console.log('Login Failed:', error)
-    });
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log('Login Failed:', error)
+  });
 
-    useEffect(
-        () => {
-            if (user) {
-                axios
-                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                        headers: {
-                            Authorization: `Bearer ${user.access_token}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                    .then((res) => {
-                        setProfile(res.data);
-                        sessionStorage.setItem('user', res.data.id)
-                    })
-                    .catch((err) => console.log(err));
-            }
-        },
-        [ user ]
-    );
-
-    // log out function to log the user out of google and set the profile array to null
-    const logOut = () => {
-        googleLogout();
-        setProfile(null);
-        sessionStorage.removeItem('user')
-    };
-
-    
-    const router = createBrowserRouter([
-        {
-          path: "/",
-          element: <Profile profile={profile} logOut={logOut}/>,
+  useEffect(() => {
+    if (user) {
+      axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+        headers: {
+          Authorization: `Bearer ${user.access_token}`,
+          Accept: 'application/json'
         }
-      ]);
+      })
+      .then((res) => {
+        setProfile(res.data);
+        sessionStorage.setItem('user', res.data.id)
+      })
+      .catch((err) => console.error('Error fetching profile:', err));
+    }
+  }, [user]);
 
-    return (
-        <div>
-            <Header logOut={logOut} login={login} profile={profile}/>
-            {profile ? (
-                 <RouterProvider router={router} />
-            ) : (
-                <button onClick={() => login()}>Sign in with Google 🚀 </button>
-            )}
-        </div>
-        
-    );
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+    sessionStorage.removeItem('user');
+  };
+
+  return (
+    <Router>
+      <div>
+        <Header logOut={logOut} login={login} profile={profile}/>
+        <Routes>
+          {profile ? (
+            <Route path="/profile" element={<Profile profile={profile} logOut={logOut}/>} />
+          ) : (
+            <Route path="*" element={<button onClick={() => login()}>Sign in with Google 🚀 </button>} />
+          )}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
 export default App;
